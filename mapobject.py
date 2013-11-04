a45e54c5d1d3958dd2531018ecca42063296c427
Tondo <tatsuya.ahad@gmail.com>
2013-11-03 20:33:30 -0500

    Created MapObject

---
+++ mapobject.py
@@ -0,0 +1,43 @@
+class ObjTypeEnum(object):
+    OBJ_TYPE_PASSABLE = 'p'
+    OBJ_TYPE_IMPASSABLE = 'i'
+    OBJ_TYPE_SEMIPASSABLE = 's'
+
+
+class Position(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "<Position x:%s y:%s>" % (self.x, self.y)
+
+
+class Dimension(object):
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "<Dimension w:%s h:%s>" % (self.w, self.h)
+
+
+class MapObject(object):
+    def __init__(self, name, type, pos, dim):
+        self.name = name
+        self.type = type
+        self.pos = pos
+        self.dim = dim
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return ("<MapObject name:%s type:%s pos:%s dim:%s>"
+                % (self.name, self.type, self.pos, self.dim))
--- objimporter.py
+++ objimporter.py
@@ -1,12 +1,35 @@
 import json
+from mapobject import *
+
 
 class ObjImporter(object):
     @staticmethod
     def readFile(fileName):
         json_file = open(fileName)
-        data = json.load(json_file)
+        json_data = json.load(json_file)
         json_file.close()
-        return data
+        objList = []
+
+        for data in json_data:
+            name = data["name"]
+            pos = Position(data["pos"]["x"], data["pos"]["y"])
+            dim = Dimension(data["dim"]["w"], data["dim"]["h"])
+            type = None
+
+            if data["type"] == "passable":
+                type = ObjTypeEnum.OBJ_TYPE_PASSABLE
+            elif data["type"] == "impassable":
+                type = ObjTypeEnum.OBJ_TYPE_IMPASSABLE
+            elif data["type"] == "semipassable":
+                type = ObjTypeEnum.OBJ_TYPE_SEMIPASSABLE
+            else:
+                print "Invalid type in Json file"
+                return None
+
+            obj = MapObject(name, type, pos, dim)
+            objList.append(obj)
+
+        return objList
 
 if __name__ == "__main__":
     data = ObjImporter.readFile("objdefines.json")
