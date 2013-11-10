1ef128306aa0a630582ed593691c59704966cf1d
Tondo <tatsuya.ahad@gmail.com>
2013-11-10 14:24:27 -0500

    Fixed misalignment between box2d coordinate and sprite coordinates.

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ pickups = []
 data = ObjImporter.readFile("objdefines.json")
 for o in data:
     if o.name == "building":
-        pos = b2Vec2(o.pos.x * TILE_SIZE, o.pos.y * TILE_SIZE)
-        dim = b2Vec2(o.dim.w * TILE_SIZE, o.dim.h * TILE_SIZE)
+        pos = b2Vec2(o.pos.x * TILE_SIZE + o.dim.w * TILE_SIZE / 2, -o.pos.y * TILE_SIZE - o.dim.h * TILE_SIZE / 2)
+        dim = b2Vec2(o.dim.w * TILE_SIZE / 2, o.dim.h * TILE_SIZE / 2)
         bodyDef = b2BodyDef()
         bodyDef.type = b2_staticBody
         bodyDef.userData = o
@@ -140,7 +140,7 @@ for o in data:
         fixture.userData = o
         objects.append(body)
     elif o.name == "pickup":
-        pos = b2Vec2(o.pos.x * TILE_SIZE, o.pos.y * TILE_SIZE)
+        pos = b2Vec2(o.pos.x * TILE_SIZE, -o.pos.y * TILE_SIZE)
         dim = b2Vec2(o.dim.w * TILE_SIZE, o.dim.h * TILE_SIZE)
         bodyDef = b2BodyDef()
         bodyDef.type = b2_staticBody
@@ -159,7 +159,7 @@ for o in data:
 controlState = 0
 
 size = (160, 144)
-scale = 1
+scale = 2
 scaledSize = (size[0] * scale, size[1] * scale)
 window = display.set_mode(scaledSize)
 gbScreen = pygame.Surface(size)
@@ -177,7 +177,9 @@ for key in sprites.keys():
     sprite_dir += key
     sprites[key] = pygame.image.load(sprite_dir).convert()
 
-car.m_body.position = b2Vec2(25 * TILE_SIZE, -25 * TILE_SIZE)
+sprites["car.png"] = pygame.image.load("./sprite/car.png").convert()
+
+#car.m_body.position = b2Vec2(25 * TILE_SIZE, -25 * TILE_SIZE)
 
 while True:
     diff = (1000.0 / 59.7)
@@ -224,33 +226,6 @@ while True:
     topLeft[0] = int(position.x / TILE_SIZE - 15)
     topLeft[1] = int(-position.y / TILE_SIZE - 15)
 
-    for x in xrange(topLeft[0], topLeft[0] + 30):
-        for y in xrange(topLeft[1], topLeft[1] + 27):
-            if len(grid) <= y or len(grid[y]) <= x or y < 0 or x < 0:
-                continue
-
-            sprite = pygame.sprite.Sprite()
-            sprite.image = sprites[grid[y][x].getSpriteName()]
-            drawPos = [x * TILE_SIZE * PPM, y * TILE_SIZE * PPM,
-                       TILE_SIZE * PPM, TILE_SIZE * PPM]
-            drawPos[0] += offset[0]
-            drawPos[1] += offset[1]
-            sprite.rect = sprite.image.get_rect()
-            sprite.rect.topleft = [drawPos[0], drawPos[1]]
-            gbScreen.blit(sprite.image, sprite.rect)
-
-    for body in pickups:
-        pos = body.worldCenter.copy()
-        pos.x *= PPM
-        pos.y *= -PPM
-        pos.x += offset[0]
-        pos.y += offset[1]
-        pygame.draw.circle(
-            gbScreen,
-            Palette.BRIGHT,
-            (int(pos.x), int(pos.y)),
-            int(body.fixtures[0].shape.radius * PPM))
-
     for body in (car.GetAllBodies() + objects):  # or: world.bodies
         # The body gives us the position and angle of its shapes
         for fixture in body.fixtures:
@@ -264,7 +239,7 @@ while True:
             # factor.
             vertices = [(body.transform * v) * PPM for v in shape.vertices]
 
-            # But wait! It's upside-down! Pygame and Box2D orient their
+            ## But wait! It's upside-down! Pygame and Box2D orient their
             # axes in different ways. Box2D is just like how you learned
             # in high school, with positive x and y directions going
             # right and up. Pygame, on the other hand, increases in the
@@ -275,6 +250,42 @@ while True:
                  - v[1] + offset[1]) for v in vertices]
 
             pygame.draw.polygon(gbScreen, Palette.NORM, vertices)
+
+    for x in xrange(topLeft[0], topLeft[0] + 30):
+        for y in xrange(topLeft[1], topLeft[1] + 27):
+            if (y < 0 or x < 0 or len(grid) <= y or len(grid[y]) <= x):
+                continue
+            if isinstance(grid[y][x], str):
+                continue
+
+            sprite = pygame.sprite.Sprite()
+            sprite.image = sprites[grid[y][x].getSpriteName()]
+            drawPos = [x * TILE_SIZE * PPM, y * TILE_SIZE * PPM,
+                       TILE_SIZE * PPM, TILE_SIZE * PPM]
+            drawPos[0] += offset[0]
+            drawPos[1] += offset[1]
+            sprite.rect = sprite.image.get_rect()
+            sprite.rect.topleft = [drawPos[0], drawPos[1]]
+            gbScreen.blit(sprite.image, sprite.rect)
+
+    sprite = pygame.sprite.Sprite()
+    sprite.image = sprites["car.png"]
+    sprite.rect = sprite.image.get_rect()
+    sprite.rect.topleft = [position[0] + offset[0] - 12, -position[1] + offset[1] - 15]
+    gbScreen.blit(sprite.image, sprite.rect)
+
+    for body in pickups:
+        pos = body.worldCenter.copy()
+        pos.x *= PPM
+        pos.y *= -PPM
+        pos.x += offset[0]
+        pos.y += offset[1]
+        pygame.draw.circle(
+            gbScreen,
+            Palette.BRIGHT,
+            (int(pos.x), int(pos.y)),
+            int(body.fixtures[0].shape.radius * PPM))
+
 
     textSystem.update(diff)
     textSystem.drawText(gbScreen)
